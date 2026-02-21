afd181b5a748af71f32cddbff3051c8812943830
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2026-02-22 02:09:48 +0530

    Add files via upload

---
+++ E-commerceMockito/src/main/java/com/test/spring/E_commerceMockito/App.java
@@ -0,0 +1,10 @@
+package com.test.spring.E_commerceMockito;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
